c5b25cda79a18be3dafdc3407435dd15886e1fbb
suparth <suparthnarayanghimire2014@gmail.com>
2023-06-04 00:22:50 +0545

    fix: fix z-index

--- src/components/Atoms/Modal/Modal.tsx
+++ src/components/Atoms/Modal/Modal.tsx
@@ -10,7 +10,7 @@ const Modal: React.FC<Props> = (props) => {
   if (!props.opened) return null;
 
   return (
-    <div className="absolute w-screen min-h-screen h-full bg-neutral-800 rounded top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] p-3 grid grid-rows-[max-content_1fr] gap-5">
+    <div className="absolute w-screen min-h-screen h-full bg-neutral-800 rounded top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] p-3 grid grid-rows-[max-content_1fr] gap-5 z-[99999]">
       <div className="w-full">
         <div className="flex justify-between">
           {props.title && <h2 className="text-xl">{props.title}</h2>}
